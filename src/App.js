04c2b2678872f031141b265fa5bd8880a0ad734a
Daris Mavric <mvrcdaris@gmail.com>
2024-04-26 20:41:58 +0200

    Zavrsen Leaderboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AddCourse from "./pages/AddCourse";
+import LeaderboardPage from "./pages/LeaderboardPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
         <Route path="/addcourse" element={<AddCourse />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/LeaderboardPage.js
@@ -0,0 +1,49 @@
+import NavigationCard from "../components/CourseCards/NavigationCard";
+import styles from "../styles/Leaderboard.css";
+
+const LeaderboardPage = () => {
+  return (
+    <div className="pre-leaderboard">
+      <NavigationCard />
+      <div className="leaderboard">
+        <div className="first-three">
+          <div className="silver">
+            <h1>Daris Mavric</h1>
+          </div>
+          <div className="gold">
+            <h1>Daris Mavric</h1>
+          </div>
+          <div className="bronze">
+            <h1>Daris Mavric</h1>
+          </div>
+        </div>
+        <div className="sections">
+          <h1>Rank</h1>
+          <h1>Name</h1>
+          <h1>Finished Courses</h1>
+          <h1>Points</h1>
+        </div>
+        <div className="student">
+          <h3>1</h3>
+          <h3>Daris Mavric</h3>
+          <h3>20</h3>
+          <h3>1208</h3>
+        </div>
+        <div className="student">
+          <h3>1</h3>
+          <h3>Daris Mavric</h3>
+          <h3>20</h3>
+          <h3>1208</h3>
+        </div>
+        <div className="student">
+          <h3>1</h3>
+          <h3>Daris Mavric</h3>
+          <h3>20</h3>
+          <h3>1208</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeaderboardPage;
