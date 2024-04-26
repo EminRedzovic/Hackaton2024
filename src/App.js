666f5ff9165f072cc4b6ffbe6d65dcc83d081791
Daris Mavric <mvrcdaris@gmail.com>
2024-04-26 21:32:20 +0200

    Started Creating Profile Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AddCourse from "./pages/AddCourse";
 import LeaderboardPage from "./pages/LeaderboardPage";
+import ProfilePage from "./pages/profilePage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/addcourse" element={<AddCourse />} />
       </Routes>
     </BrowserRouter>
