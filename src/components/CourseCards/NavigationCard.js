230b8b749f5ed7a8da36cde6179c0224cac835fb
EminRedzovic <redzovicemin200@gmail.com>
2024-04-27 03:55:24 +0200

    fix

--- src/components/CourseCards/NavigationCard.js
+++ src/components/CourseCards/NavigationCard.js
@@ -11,14 +11,10 @@ import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 const NavigationCard = () => {
-  const [isOpen, setIsOpen] = useState(true);
   const [user, setUser] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
   const getUser = async (uid) => {
     const result = await getUserData(uid);
     setUser(result);
@@ -44,81 +40,85 @@ const NavigationCard = () => {
 
   return (
     <>
-      <Box className={`sidebar ${isOpen ? "open" : "closed"}`}>
-        {isOpen && (
-          <Box>
-            <Box className={`logo`}>
-              <img src={sajtLogo} alt="Profile Picture" />
-              <Typography
-                sx={{
-                  letterSpacing: 4,
-                  fontWeight: "bold",
-                  fontSize: 20,
-                  paddingLeft: 2,
-                }}
-              >
-                EduConnect
-              </Typography>
-            </Box>
-
-            {user && (
-              <>
-                <Box className={`badges `}>
-                  <Typography>Badges : </Typography>
-                  <Box className="badges-container">
-                    <MilitaryTechIcon />
-                    <MilitaryTechIcon />
-                    <MilitaryTechIcon />
-                    <MilitaryTechIcon />
-                  </Box>
-                </Box>
-                <ul className={`navigation-items`}>
-                  <li>Kursevi</li>
-                  <li>Downloaded Lessons</li>
-                </ul>
-              </>
-            )}
-            {user ? (
-              <button className="logoutButton" onClick={logout}>
-                Log out
-              </button>
-            ) : (
-              <Typography
-                style={{
-                  marginTop: 20,
-                  marginLeft: 10,
-                }}
-              >
-                <span
-                  onClick={() => {
-                    navigate("/loginpage");
-                  }}
-                  style={{ cursor: "pointer" }}
-                >
-                  Login
-                </span>{" "}
-                or{" "}
-                <span
-                  onClick={() => {
-                    navigate("/registerPage");
-                  }}
-                  style={{ cursor: "pointer" }}
-                >
-                  Register
-                </span>{" "}
-                to use all features
-              </Typography>
-            )}
+      <Box className={"sidebar"}>
+        <Box>
+          <Box className={`logo`}>
+            <img src={sajtLogo} alt="Profile Picture" />
+            <Typography
+              sx={{
+                letterSpacing: 4,
+                fontWeight: "bold",
+                fontSize: 20,
+                paddingLeft: 2,
+              }}
+            >
+              EduConnect
+            </Typography>
           </Box>
-        )}
-        <IconButton className={`close-button`} onClick={toggleSidebar}>
-          {isOpen ? (
-            <ArrowBackIcon sx={{ color: "white", marginLeft: 1 }} />
-          ) : (
-            <ArrowForwardIcon sx={{ color: "white", marginLeft: -1 }} />
+
+          {user && (
+            <>
+              <Box className={`badges `}>
+                <Typography>Badges : </Typography>
+                <Box className="badges-container">
+                  <MilitaryTechIcon />
+                  <MilitaryTechIcon />
+                  <MilitaryTechIcon />
+                  <MilitaryTechIcon />
+                </Box>
+              </Box>
+              <ul className={`navigation-items`}>
+                <li
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                >
+                  Kursevi
+                </li>
+                <li
+                  onClick={() => {
+                    navigate("/leaderboard");
+                  }}
+                >
+                  Leaderboard
+                </li>
+              </ul>
+            </>
           )}
-        </IconButton>
-        {user && isOpen && (
+          {user ? (
+            <button className="logoutButton" onClick={logout}>
+              Log out
+            </button>
+          ) : (
+            <Typography
+              style={{
+                marginTop: 20,
+                marginLeft: 10,
+              }}
+            >
+              <span
+                onClick={() => {
+                  navigate("/loginpage");
+                }}
+                style={{ cursor: "pointer" }}
+              >
+                Login
+              </span>{" "}
+              or{" "}
+              <span
+                onClick={() => {
+                  navigate("/registerPage");
+                }}
+                style={{ cursor: "pointer" }}
+              >
+                Register
+              </span>{" "}
+              to use all features
+            </Typography>
+          )}
+        </Box>
+
+        {user && (
           <Box className={`profile`}>
             <img src={mockedProfile} alt="Profile Picture" />
             <span>{user && user.displayName}</span>
