88660f1f8f305159fffec868a307964c2bc12436
Daris Mavric <mvrcdaris@gmail.com>
2024-04-27 03:10:57 +0200

    Implementing system for changing user info

--- src/pages/profilePage.js
+++ src/pages/profilePage.js
@@ -2,8 +2,42 @@ import "../styles/profilePage.css";
 import NavigationCard from "../components/CourseCards/NavigationCard";
 import profile from "./profile.jpg";
 import medalja from "./medalja.svg";
+import { CiSettings } from "react-icons/ci";
+import { auth, getUserData } from "../firebase";
+import { useState, useEffect } from "react";
 
 const ProfilePage = () => {
+  const [user, setUser] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    console.log(user);
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        getUser(authUser.uid);
+      }
+
+      if (isLoading) {
+        setIsLoading(false);
+      }
+    });
+    return () => unsubscribe();
+  }, [auth, isLoading]);
+  const getUser = async (uid) => {
+    const result = await getUserData(uid);
+    setUser(result);
+  };
+  const changeProfile = () => {};
+  const editProfile = () => {
+    console.log(user.userId);
+    let modal = document.querySelector(".editProfile");
+    console.log(modal.style.display);
+    if (modal.style.display === "" || modal.style.display === "none") {
+      modal.style.display = "block";
+    } else {
+      modal.style.display = "none";
+    }
+  };
   return (
     <div className="pre-section">
       <NavigationCard />
@@ -11,7 +45,11 @@ const ProfilePage = () => {
         <div className="about-me-section">
           <div className="image-and-name">
             <img src={profile} alt="asdasd" />
-            <h1>Daris Mavric</h1>
+            <h1>{user && user.displayName}</h1>
+            <CiSettings
+              style={{ width: "40px", height: "40px", marginLeft: "10px" }}
+              onClick={() => editProfile()}
+            />
           </div>
           <p>1028 Poena</p>
         </div>
@@ -52,6 +90,16 @@ const ProfilePage = () => {
             </li>
           </ul>
         </div>
+      </div>
+      <div className="editProfile">
+        <p onClick={() => editProfile()}>X</p>
+        <img src={profile} />
+        <input type="file" />
+        <label>First and Last Name</label>
+        <input type="text" placeholder="Daris Mavric" />
+        <label>About me</label>
+        <textarea placeholder="Write something about you" />
+        <button onClick={() => changeProfile()}>Save</button>
       </div>
     </div>
   );
